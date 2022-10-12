06638728e06c628fb68d5e8231a94403111a21e0
Shivam Chaudhary <shivamc@nvidia.com>
2022-10-12 11:57:42 -0700

    GH Specific Infrastructure for IPMI-OEM commands.

---
+++ Grace/src/ghoemcommands.cpp
@@ -0,0 +1,88 @@
+/**
+ * Copyright © 2022 NVIDIA Corporation
+ * 
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+#include "ghoemcommands.hpp"
+
+#include <bits/stdc++.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <security/pam_appl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/process/child.hpp>
+#include <ipmid/api-types.hpp>
+#include <ipmid/api.hpp>
+#include <ipmid/utils.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Software/Activation/server.hpp>
+#include <xyz/openbmc_project/Software/Version/server.hpp>
+
+#include <algorithm>
+#include <array>
+#include <filesystem>
+#include <string>
+#include <tuple>
+#include <vector>
+
+// IPMI OEM Major and Minor version
+static constexpr uint8_t OEM_MAJOR_VER = 0x01;
+static constexpr uint8_t OEM_MINOR_VER = 0x00;
+
+void registerNvOemFunctions() __attribute__((constructor));
+
+using namespace phosphor::logging;
+
+using GetSubTreeType = std::vector<
+    std::pair<std::string,
+        std::vector<std::pair<std::string, std::vector<std::string>>>>>;
+using GetSubTreePathsType = std::vector<std::string>;
+using BasicVariantType = std::variant<std::string>;
+using PropertyMapType =
+    boost::container::flat_map<std::string, BasicVariantType>;
+
+namespace ipmi
+{
+ipmi::RspType<
+    uint8_t,  // Major Version
+    uint8_t  // Minor Version
+    > ipmiGetOEMVersion()
+{
+    return ipmi::responseSuccess(OEM_MAJOR_VER, OEM_MINOR_VER);
+}
+
+}
+
+
+
+void registerNvOemFunctions()
+{
+
+// <Get IPMI OEM Version>
+    log<level::NOTICE>(
+        "Registering ", entry("NetFn:[%02Xh], ", ipmi::nvidia::netFnOemNV),
+        entry("Cmd:[%02Xh]", ipmi::nvidia::misc::cmdGetOEMVersion));
+
+    ipmi::registerHandler(ipmi::prioOemBase, ipmi::nvidia::netFnOemNV,
+                          ipmi::nvidia::misc::cmdGetOEMVersion,
+                          ipmi::Privilege::User,
+                          ipmi::ipmiGetOEMVersion);
+
+}
