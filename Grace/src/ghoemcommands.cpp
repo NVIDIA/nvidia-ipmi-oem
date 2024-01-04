c6a7c73e64a7b0aaa93d724349c9d8ee8704cd7d
Tony Ao <tao@nvidia.com>
2024-01-03 21:22:59 -0800

    add an option to disable SMBPBI passthrough commands add an option called disable-smbpbi-passthru to disable SMBPBI passthrough commands. SMBPBI passthrough commands are enabled by default. If the platform doesn't want to support the commands, we provide the option for configuration.
    NetFn 0x3c, Cmd 0x09 SMBPBI passthrough command
    NetFn 0x3c, Cmd 0x0a SMBPBI passthrough command for extended data
    
    Fixes jira https://jirasw.nvidia.com/browse/DGXOPENBMC-10233
    
    ```
    check if we get the invalid command response
    before:
    root@oberon-bmc:~# ipmitool raw 0x3c 0x09
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0x9 rsp=0xc7): Request data length invalid
    root@oberon-bmc:~# ipmitool raw 0x3c 0x0a
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0xa rsp=0xc7): Request data length invalid
    
    after:
    root@oberon-bmc:~# ipmitool raw 0x3c 0x09
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0x9 rsp=0xc1): Invalid command
    root@oberon-bmc:~# ipmitool raw 0x3c 0x0a
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0xa rsp=0xc1): Invalid command
    ```
    
    ```
    check if the change would not affect other platforms
    before:
    root@legocg1:~# ipmitool raw 0x3c 0x09
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0x9 rsp=0xc7): Request data length invalid
    root@legocg1:~# ipmitool raw 0x3c 0x0a
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0xa rsp=0xc7): Request data length invalid
    
    after:
    root@legocg1:~# ipmitool raw 0x3c 0x09
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0x9 rsp=0xc7): Request data length invalid
    root@legocg1:~# ipmitool raw 0x3c 0x0a
    Unable to send RAW command (channel=0x0 netfn=0x3c lun=0x0 cmd=0xa rsp=0xc7): Request data length invalid
    ```

--- Grace/src/ghoemcommands.cpp
+++ Grace/src/ghoemcommands.cpp
@@ -2124,6 +2124,8 @@ void registerNvOemFunctions()
                           ipmi::nvidia::misc::cmdGetOEMVersion,
                           ipmi::Privilege::User, ipmi::ipmiGetOEMVersion);
 
+ #ifdef ENABLE_SMBPBI_PASSTHRU
+    #if ENABLE_SMBPBI_PASSTHRU == 1
     // <Execute SMBPBI passthrough command>
     log<level::NOTICE>(
         "Registering ", entry("NetFn:[%02Xh], ", ipmi::nvidia::netFnOemNV),
@@ -2143,6 +2145,8 @@ void registerNvOemFunctions()
                           ipmi::nvidia::misc::cmdSMBPBIPassthroughExtended,
                           ipmi::Privilege::Admin,
                           ipmi::ipmiSMBPBIPassthroughExtendedCmd);
+    #endif
+#endif
 
     // <Set fan control mode>
     log<level::NOTICE>("Registering ",
